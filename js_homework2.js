1d0493f9bd94d4c198c422917a8aacd8502fc530
Daria <dashaalex394@gmail.com>
2024-02-10 14:41:18 +0100

    modified homework2

--- js_homework2.js
+++ js_homework2.js
@@ -2,15 +2,20 @@ let age = prompt("Вкажіть ваш вік", "");
 
 let end = '';
 
-if ((age % 100 >= 11 && age % 100 <= 14) || age % 10 === 0 || (age % 10 >= 5 && age % 10 <= 9)) {
+if(age === '') {
+    console.log ('Вкажіть ваш вік, будь ласка!')
+} else if (age < 0) {
+    console.log ('Вік не може бути менше 0')
+} else if ((age % 100 >= 11 && age % 100 <= 14) || age % 10 === 0 || (age % 10 >= 5 && age % 10 <= 9)) {
     end = 'років';
+    console.log(age + ' ' + end);
 } else if (age % 10 === 1) {
     end = 'рік';
+    console.log(age + ' ' + end);
 } else if (age % 10 === 2 || age % 10 === 3 || age % 10 === 4) {
     end = 'роки';
-}
-
-console.log(age + ' ' + end);
+    console.log(age + ' ' + end);
+} 
 
 
 
